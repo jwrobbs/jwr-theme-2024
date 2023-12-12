ab21d2b712c123336bb4062e08d7349a5b6693d3
Josh Robbs <josh@joshrobbs.com>
2023-12-12 14:51:46 -0500

    Initial index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template index file
+ *
+ * @since 20231212
+ * @author Josh Robbs <josh@joshrobbs.com>
+ * @package JWR_Theme_2024
+ */
+
+namespace JWR_Theme_2024;
+
+defined( 'ABSPATH' ) || exit;
+
+\get_header();
+\the_title();
+\the_content();
+\get_footer();
