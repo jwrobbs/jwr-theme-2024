28b1dae4259a87f5b211e07dd2dbeeaa1fc81d45
Josh Robbs <josh@joshrobbs.com>
2023-12-12 14:45:12 -0500

    Initial footer created

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * Yes, stolen from Twenty Seventeen.
+ *
+ * @package JWR_Theme_2024
+ * @since 20231212
+ */
+
+?>
+
+		</div><!-- #content -->
+
+		<footer id="colophon" class="site-footer">
+			<div class="wrap">
+					<H2>JWR FOOTER</H2>
+			</div><!-- .wrap -->
+		</footer><!-- #colophon -->
+	</div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
